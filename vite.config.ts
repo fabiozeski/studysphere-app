68eec261797ddcb21784e7c402afe313576221f6
fabiozeski <fabiozesk@gmail.com>
2025-08-16 02:12:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // ok, mantém
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 5173, // qualquer porta para DEV
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: 3003, // ***Produção vai rodar aqui***
   },
 }));
